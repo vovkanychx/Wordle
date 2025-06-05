825bb77ab5ab041ac6ae2fd6cc7a3b2173cb6348
Vovkanych Dmytro <dima.vovkanych24@gmail.com>
2025-06-05 23:33:27 +0300

    Styles fix and script added event listener

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-document.querySelectorAll('.cell').forEach(cell => ({
-    
-}))
+document.querySelectorAll('.cell').forEach((cell, index) => {
+    cell.addEventListener('keyup', e => {
+        if (document.querySelector('.grid').lastElementChild.value.length === 1) { //stop keyup event listener at the last cell when it's filled
+            return
+        }
+        if (e.target.value.length === 1) { // focus on the next cell when current cell is filled
+            document.querySelectorAll('.cell')[index].nextElementSibling.focus();
+        }
+    })
+})
